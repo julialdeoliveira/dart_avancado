2753ae179accd346be81d57623184ba2472f9ee3
unknown <julia_l_de-oliveira@estudante.sc.senai.br>
2022-07-26 21:21:39 -0300

    fixing

--- clean_arch/lib/core/get_it.dart
+++ clean_arch/lib/core/get_it.dart
@@ -5,7 +5,7 @@ import 'package:get_it/get_it.dart';
 import '../app/domain/usecases/get_all_products/get_all_products_usecase_imp.dart';
 import '../app/domain/usecases/get_product_by_id/get_product_by_id_usecase_imp.dart';
 import '../app/infra/datasources/local/get_all_products_local_datasource_imp.dart';
-import '../app/infra/repositories/get_all_product_repository_imp.dart';
+import '../app/infra/repositories/get_all_products_repository_imp.dart';
 import '../app/infra/repositories/get_product_by_id_repository_imp.dart';
 
 final getIt = GetIt.instance;
